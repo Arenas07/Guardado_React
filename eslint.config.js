810abe4194d4164f1847e564f7cd64fe5b4eac5d
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-06-10 17:18:21 -0500

    feat: :package: I added eslint, prettier and rechart packages.

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,44 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended', // Asegura que Prettier se ejecute como regla de ESLint
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    '@typescript-eslint',
+    'prettier', // Añade Prettier como plugin
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto', // Ayuda con diferencias de sistema operativo
+      },
+    ],
+    'react/react-in-jsx-scope': 'off', // No es necesario con React 17+ y la nueva JSX transform
+    'react/prop-types': 'off', // Deshabilita si usas TypeScript para validación de props
+    // Añade aquí reglas específicas de ESLint que quieras sobrescribir o añadir
+  },
+  settings: {
+    react: {
+      version: 'detect', // Detecta automáticamente la versión de React
     },
   },
-)
+};
