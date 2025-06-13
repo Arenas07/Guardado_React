87d309130e31d3a8e9b3aca1af17bb520f9ff5ea
Arenas07 <189859279+Arenas07@users.noreply.github.com>
2025-06-13 11:55:52 -0500

    feat: :heavy_plus_sign: Actualizacion de dependencias

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,21 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: 'class', // importante
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-    theme: {
-      extend: {
-        colors: {
-          background: "rgb(var(--background-1) / <alpha-value>)",
-          background2: "rgb(var(--background-2) / <alpha-value>)",
-          darkGray: "rgb(var(--light-dark-container) / <alpha-value>)",
-          darkGreen: "rgb(var(--dark-green) / <alpha-value>)",
-          lightGreen: "rgb(var(--light-green) / <alpha-value>)"
-        },
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        background: "rgb(var(--background-1) / <alpha-value>)",
+        background2: "rgb(var(--background-2) / <alpha-value>)",
+        darkGray: "rgb(var(--light-dark-container) / <alpha-value>)",
+        darkGreen: "rgb(var(--dark-green) / <alpha-value>)",
+        lightGreen: "rgb(var(--light-green) / <alpha-value>)",
+        contrast: "rba(var(--default-contrast) / <alpha-value>)",
       },
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [],
+};
+
+export default config;
