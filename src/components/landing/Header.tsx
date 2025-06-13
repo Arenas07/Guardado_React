3fdec0a86aa9ec7ddd4bd5e60e60eca135def180
Arenas07 <189859279+Arenas07@users.noreply.github.com>
2025-06-13 12:00:54 -0500

    feat: :lipstick: Adaptacion de estilos en diferentes modos

--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -1,10 +1,12 @@
-import ThemeButton from '../ThemeButton'
+import ThemeButton from '../commons/ThemeButton'
+import '../../styles/global.css'
 
 function Header() {
   return (
     <header className="py-6 px-14 flex justify-between items-center flex-row">
       <div>
-      <h2 className="text-darkGray dark:text-lightGreen text-xl">Habita</h2>
+      <h2 className="text-[rgb(var(--default-contrast))] text-xl">Habita</h2>
+
 
       </div>
       <div className="flex items-center justify-center">
