21afe200c121643b0465ae92b70cc52bc3a24640
Arenas07 <189859279+Arenas07@users.noreply.github.com>
2025-06-14 15:30:26 -0500

    feat: :sparkles: Landing improvement due to changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import Header from './components/landing/Header'
-import './styles/global.css'
-import './styles/index.css'
+import Header from './components/landing/Header.tsx';
+import Introduction from './components/landing/Introduction.tsx';
+import './styles/global.css';
+import './styles/index.css';
 
 function App() {
     
   return (
     <>
       <Header />
+      <Introduction />
     </>
   )
 }
--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -1,16 +1,21 @@
-import ThemeButton from '../commons/ThemeButton'
+import ThemeButton from '../commons/ThemeButton.tsx'
 import '../../styles/global.css'
+
 
 function Header() {
   return (
-    <header className="py-6 px-14 flex justify-between items-center flex-row">
+    <header className="top-0 w-full py-4 px-14 flex justify-between items-center flex-row bg-[rgb(var(--background-2))]">
       <div>
-      <h2 className="text-[rgb(var(--default-contrast))] text-xl">Habita</h2>
+      <span className="text-2xl font-extrabold bg-gradient-to-r from-emerald-500 via-sky-500 to-indigo-500 bg-clip-text text-transparent tracking-wider drop-shadow-sm">
+        Habita
+      </span>
+
 
 
       </div>
       <div className="flex items-center justify-center">
         <ThemeButton />
+        
       </div>
     </header>
   );
